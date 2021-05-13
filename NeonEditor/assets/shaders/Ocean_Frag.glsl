9f3abf048e27ffa8f37ebc2acc412187ee1e684f
fhusnjak <filip.husnjak@fer.hr>
2021-05-13 18:33:16 +0200

    Implement new ocean shading

--- NeonEditor/assets/shaders/Ocean_Frag.glsl
+++ NeonEditor/assets/shaders/Ocean_Frag.glsl
@@ -38,73 +38,6 @@ const float Gamma = 2.2;
 const float Epsilon = 0.00001;
 const vec3 FDielectric = vec3(0.04);
 
-struct
-{
-    vec4 Albedo;
-    vec3 Normal;
-    float Metalness;
-    float Roughness;
-} PBRProperties;
-
-// Normal distribution function
-float NDFTrowbridgeReitzGGX(vec3 N, vec3 H, float roughness)
-{
-    float alpha = roughness * roughness;
-	float alphaSq = alpha * alpha;
-
-    float NdotH = max(dot(N, H), 0.0);
-    float denom = NdotH * NdotH * (alphaSq - 1.0) + 1.0;
-    return alphaSq / (PI * denom * denom);
-}
-
-// Geometry function
-float GeometrySchlickGGX(float cosTheta, float k)
-{
-    return cosTheta / (cosTheta * (1.0 - k) + k);
-}
-// Take both view and light direction into account using Smith's method
-float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
-{
-    float r = roughness + 1.0;
-    float k = (r * r) / 8.0;
-    return GeometrySchlickGGX(max(dot(N, L), 0.0), k) *
-           GeometrySchlickGGX(max(dot(N, V), 0.0), k);
-}
-
-// Shlick's approximation of the Fresnel factor.
-vec3 FresnelSchlick(float cosTheta, vec3 F0)
-{
-    return F0 + (1.0 - F0) * pow (1.0 - cosTheta, 5.0);
-}
-
-vec3 Lighting(vec3 V)
-{
-    uint lightCount = clamp(u_Count, 0, MAX_LIGHT_COUNT - 1);
-    vec3 result = vec3(0.0);
-    for (int i = 0; i < lightCount; i++)
-	{
-        vec3 L = normalize(u_Lights[i].Direction.xyz);      
-        vec3 H = normalize(V + L);
-
-        // Cook-Torrance BRDF
-        vec3 F0 = mix(FDielectric, PBRProperties.Albedo.rgb, PBRProperties.Metalness);
-        float NDF = NDFTrowbridgeReitzGGX(PBRProperties.Normal, H, PBRProperties.Roughness);
-        float geometry = GeometrySmith(PBRProperties.Normal, V, L, PBRProperties.Roughness);
-        vec3 fresnel = FresnelSchlick(max(dot(H, V), 0.0), F0);
-        vec3 kD = (vec3(1.0) - fresnel) * (1.0 - PBRProperties.Metalness);
-        vec3 diffuse = kD * PBRProperties.Albedo.rgb / PI;
-                
-        vec3 numerator = NDF * geometry * fresnel;
-        float NdotL = max(dot(PBRProperties.Normal, L), 0.0);
-        float denominator = 4.0 * max(dot(PBRProperties.Normal, V), 0.0) * NdotL;
-        vec3 specular = numerator / max(denominator, Epsilon);
-
-        result += u_Lights[i].Strength * u_Lights[i].Radiance.rgb * (diffuse + specular) * NdotL;
-	}
-
-    return result;
-}
-
 void main()
 {
 	vec3 oceanColor = vec3(0.2122, 0.6105, 1.0000);
@@ -114,10 +47,44 @@ void main()
 	vec3 V = u_CameraPosition.xyz - v_WorldPosition;
 	vec3 L = reflect(-normalize(V), N);
 
-	PBRProperties.Albedo = vec4(0.05, 0.22, 0.4, 1.0);
-    PBRProperties.Normal = N;
-    PBRProperties.Metalness = 0.1;
-    PBRProperties.Roughness = max(0.5, 1.0 - 1.0 / (1.0 + length(V) * 0.001));
+	// NOTE: also defined in vertex shader
+	const vec3 sunColor			= vec3(1.0, 1.0, 0.47);
+	const vec3 perlinFrequency	= vec3(1.12, 0.59, 0.23);
+	const vec3 perlinGradient	= vec3(0.014, 0.016, 0.022);
+	const vec3 sundir			= vec3(0.603, -0.761, 0.240);
 
-	o_Color = vec4(Lighting(normalize(V)), 1.f);
+	float F0 = 0.020018673;
+	float F = F0 + (1.0 - F0) * pow(1.0 - dot(N, L), 5.0);
+
+	vec3 refl = texture(u_EnvRadianceTex, L).rgb;
+
+	// Reinhard tonemapping operator.
+	const float pureWhite = 1.0;
+	float luminance = dot(refl, vec3(0.2126, 0.7152, 0.0722));
+	float mappedLuminance = (luminance * (1.0 + luminance / (pureWhite * pureWhite))) / (1.0 + luminance);
+
+	// Scale color by ratio of average luminances.
+	vec3 mappedColor = (mappedLuminance / luminance) * refl;
+
+	// tweaked from ARM/Mali's sample
+	float turbulence = max(1.6 - grad.w, 0.0);
+	float color_mod = 1.0 + 3.0 * smoothstep(1.2, 1.8, turbulence);
+
+	// some additional specular (Ward model)
+	const float rho = 0.3;
+	const float ax = 0.2;
+	const float ay = 0.1;
+
+	vec3 h = sundir + V;
+	vec3 x = cross(sundir, N);
+	vec3 y = cross(x, N);
+
+	float mult = (ONE_OVER_4PI * rho / (ax * ay * sqrt(max(1e-5, dot(sundir, N) * dot(V, N)))));
+	float hdotx = dot(h, x) / ax;
+	float hdoty = dot(h, y) / ay;
+	float hdotn = dot(h, N);
+
+	float spec = mult * exp(-((hdotx * hdotx) + (hdoty * hdoty)) / (hdotn * hdotn));
+
+	o_Color = vec4(oceanColor + sunColor * spec, 1.0);
 }
